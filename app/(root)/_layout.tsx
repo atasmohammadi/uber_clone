d4417b298973046ed139ba42f93557c69d8a09b4
Ata Mohammadi <atasmohammadi@gmail.com>
2024-10-11 18:58:47 +0200

    update routes, user api

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router/stack";
+import { Stack } from "expo-router";
 
-export default function Layout() {
-  return <Stack />;
-}
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="find-ride" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="confirm-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="book-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
